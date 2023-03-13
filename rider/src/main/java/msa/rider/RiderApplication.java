9485cfa9523b94f7693e3b26de784b3cd7ede68e
sijun park <sionwer4@naver.com>
2023-03-13 20:41:28 +0900

    rebuild restaurant, rider service project

---
+++ rider/src/main/java/msa/rider/RiderApplication.java
@@ -0,0 +1,13 @@
+package msa.rider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderApplication.class, args);
+	}
+
+}
---
+++ rider/src/test/java/msa/rider/RiderApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.rider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
