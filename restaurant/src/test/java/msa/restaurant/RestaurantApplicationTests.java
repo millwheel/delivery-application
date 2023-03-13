57e58c952a0464e65ae086e4bd0628547a7dcdc6
sijun park <sionwer4@naver.com>
2023-03-13 19:32:34 +0900

    add customer/restarant/rider service project

---
+++ restaurant/src/test/java/msa/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
