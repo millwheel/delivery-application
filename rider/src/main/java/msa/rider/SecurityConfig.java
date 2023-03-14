a8da89e7e904a2914283e8ba0239fffe48e25b5f
Sijun <sionwer4@naver.com>
2023-03-14 14:23:05 +0900

    feat: spring security set-up for using cognito

---
+++ rider/src/main/java/msa/rider/SecurityConfig.java
@@ -0,0 +1,29 @@
+package msa.rider;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .and()
+                .authorizeRequests(authorize -> authorize.mvcMatchers("/")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+        return http.build();
+    }
+}
