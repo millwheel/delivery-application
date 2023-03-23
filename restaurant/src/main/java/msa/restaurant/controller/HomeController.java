e51bcf8dfddfbe9b229d5e83ba1a9c9181fe2806
Sijun <sionwer4@naver.com>
2023-03-23 09:55:54 +0900

    feat: Create home controller for test

---
+++ restaurant/src/main/java/msa/restaurant/controller/HomeController.java
@@ -0,0 +1,13 @@
+package msa.restaurant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Restaurant server is activated successfully";
+    }
+}
