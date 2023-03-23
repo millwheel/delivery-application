1ca7d0bc6a5ceef891c0c4e2fcda2d8586aa80f7
Sijun <sionwer4@naver.com>
2023-03-23 09:52:05 +0900

    fix: Change port setting by using application properties

--- rider/src/main/java/msa/rider/RiderApplication.java
+++ rider/src/main/java/msa/rider/RiderApplication.java
@@ -9,9 +9,7 @@ import java.util.Collections;
 public class RiderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(RiderApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
-		app.run(args);
+		SpringApplication.run(RiderApplication.class, args);
 	}
 
 }
