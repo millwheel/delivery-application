509a404327a1606db132570d1f9fc92083dc9102
Sijun <sionwer4@naver.com>
2023-04-04 15:02:40 +0900

    update before revert

--- restaurant/src/main/java/msa/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/msa/restaurant/RestaurantApplication.java
@@ -1,10 +1,7 @@
 package msa.restaurant;
 
-import msa.restaurant.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class RestaurantApplication {
