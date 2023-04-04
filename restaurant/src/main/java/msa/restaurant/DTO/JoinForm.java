32d147c4001e6b88c37c450ea03d6181b0ce80c6
Sijun <sionwer4@naver.com>
2023-04-04 15:44:30 +0900

    feat: Create DTO

---
+++ restaurant/src/main/java/msa/restaurant/DTO/JoinForm.java
@@ -0,0 +1,13 @@
+package msa.restaurant.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinForm {
+    private String name;
+    private String email;
+    private String password;
+    private String passwordConfirm;
+}
