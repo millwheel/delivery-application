3dff68eda53b03ed950414339f76370a36c5dd1d
Sijun <sionwer4@naver.com>
2023-04-04 16:41:46 +0900

    fix: delete @Id annotation in member DAO

--- restaurant/src/main/java/msa/restaurant/DAO/Member.java
+++ restaurant/src/main/java/msa/restaurant/DAO/Member.java
@@ -2,14 +2,12 @@ package msa.restaurant.DAO;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Setter
 @Getter
 public class Member {
-    @Id
     private Long id;
     private String name;
     private String email;
