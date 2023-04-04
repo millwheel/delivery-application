f5a828e372c458848307e67f144875b6809c8cc2
Sijun <sionwer4@naver.com>
2023-04-04 20:53:39 +0900

    feat: Create aws dynamodb Repository

--- customer/src/main/java/msa/customer/repository/AwsDynamoDbRepository.java
+++ customer/src/main/java/msa/customer/repository/AwsDynamoDbRepository.java
@@ -1,7 +1,29 @@
 package msa.customer.repository;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import msa.customer.DAO.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class AwsDynamoDbRepository {
+public class AwsDynamoDbRepository implements MemberRepository{
+    private final AmazonDynamoDBClient amazonDynamoDBClient;
+
+    public AwsDynamoDbRepository(AmazonDynamoDBClient amazonDynamoDBClient) {
+        this.amazonDynamoDBClient = amazonDynamoDBClient;
+    }
+
+    @Override
+    public void make(Member member) {
+        DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDBClient);
+        mapper.save(member);
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email) {
+        DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDBClient);
+        return Optional.ofNullable(mapper.load(Member.class, email));
+    }
 }
