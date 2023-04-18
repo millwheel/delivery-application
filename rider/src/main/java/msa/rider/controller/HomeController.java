3fb8fe0238e101880a7d34391852c231d1597e5e
sijun park <sionwer4@naver.com>
2023-04-18 17:38:57 +0900

    fix: change to basic text and add response status annotation

--- rider/src/main/java/msa/rider/controller/HomeController.java
+++ rider/src/main/java/msa/rider/controller/HomeController.java
@@ -1,17 +1,17 @@
 package msa.rider.controller;
 
 import msa.rider.DTO.Blog;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public Blog home(){
-        Blog blog = new Blog();
-        blog.setTitle("Server test");
-        blog.setContent("Rider server is activated successfully");
-        return blog;
+    @ResponseStatus(HttpStatus.OK)
+    public String home(){
+        return "Rider server is activated successfully";
     }
 }
