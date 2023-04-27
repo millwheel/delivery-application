a813ca3239566d73f3572066e9c3a7d2c9abc61c
sijun park <sionwer4@naver.com>
2023-04-27 11:39:55 +0900

    feat:Delete Blog data model

--- rider/src/main/java/msa/rider/controller/HomeController.java
+++ rider/src/main/java/msa/rider/controller/HomeController.java
@@ -1,16 +1,13 @@
 package msa.rider.controller;
 
-import msa.rider.DTO.Blog;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/rider")
-    public String home(){
+    public String home() {
         return "Rider server is activated successfully";
     }
 }
