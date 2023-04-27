17c9c2bc41432cd62bb0dee393082c5fa4897712
sijun park <sionwer4@naver.com>
2023-04-27 19:40:53 +0900

    fix(rider): Extract only auth header from HTTP request

--- rider/src/main/java/msa/rider/controller/ListController.java
+++ rider/src/main/java/msa/rider/controller/ListController.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import msa.rider.service.MemberService;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,29 +24,9 @@ public class ListController {
 
     @GetMapping("/rider/order-list")
     @ResponseStatus(HttpStatus.OK)
-    public String orderList (@RequestHeader Map<String, String> headers){
+    public String orderList (@RequestHeader(HttpHeaders.AUTHORIZATION) String jwt){
 
-        if (headers == null){
-            return "No Headers";
-        }
-
-        StringBuilder mapAsString = new StringBuilder("{");
-        for (String key : headers.keySet()) {
-            mapAsString.append(key + "=" + headers.get(key) + ", ");
-        }
-        mapAsString.delete(mapAsString.length()-2, mapAsString.length()).append("}");
-
-        String headerString = mapAsString.toString();
-
-        return headerString;
-
-//        String token = jwt.get();
-//        log.info("header={}", token);
-//            Claims claims = memberService.parseJwtToken(token);
-//            log.info("claims={}", claims);
-//            String email = memberService.getEmailFromClaims(claims);
-//            log.info("email={}", email);
-//            return "Your token is " + token;
+        return jwt;
     }
 
 }
