918931fcadb46f5f4c8f915775076548ab7e6abf
Sijun <sionwer4@naver.com>
2023-05-01 19:23:00 +0900

    fix(customer): Delete cassandra and Change to dynamo

--- customer/src/main/java/msa/customer/DAO/Member.java
+++ customer/src/main/java/msa/customer/DAO/Member.java
@@ -3,22 +3,15 @@ package msa.customer.DAO;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
 
-@Table("member")
+
 @Getter
 @Setter
 public class Member {
 
-    @PrimaryKey
     private Long id;
-    @Column
     private String name;
-    @Column
     private String email;
-    @Column
     private String password;
 
     public Member() {
