531e69bdf23b657fab735558d9c4cb621a483b8e
Sijun <sionwer4@naver.com>
2023-05-01 20:23:09 +0900

    feat(customer): Create employee and department DAO

---
+++ customer/src/main/java/msa/customer/DAO/Department.java
@@ -0,0 +1,19 @@
+package msa.customer.DAO;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamoDBDocument
+public class Department {
+    @DynamoDBAttribute
+    private String departmentName;
+
+    @DynamoDBAttribute
+    private String departmentCode;
+}
