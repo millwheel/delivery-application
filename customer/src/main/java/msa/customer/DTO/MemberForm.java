849ac8c04dc66564a781900b76532c71f136ea8c
Sijun <sionwer4@naver.com>
2023-05-03 16:17:18 +0900

    feat(customer):Create DTO member form

---
+++ customer/src/main/java/msa/customer/DTO/MemberForm.java
@@ -0,0 +1,15 @@
+package msa.customer.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MemberForm {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+}
