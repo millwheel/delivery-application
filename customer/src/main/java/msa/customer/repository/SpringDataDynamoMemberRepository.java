9f67c4aa1c88b2a90b05816a21125cb03c9f323f
sijun park <sionwer4@naver.com>
2023-05-05 11:42:46 +0900

    feat(customer): Add spring data test query method

--- customer/src/main/java/msa/customer/repository/SpringDataDynamoMemberRepository.java
+++ customer/src/main/java/msa/customer/repository/SpringDataDynamoMemberRepository.java
@@ -4,9 +4,17 @@ import msa.customer.DAO.Member;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @EnableScan
 public interface SpringDataDynamoMemberRepository extends CrudRepository<Member, String> {
+
+    Optional<String> findByEmail(String email);
+    long countByLastname(String lastname);
+
+    List<Member> removeByFirstname(String firstname);
+
+    boolean findByAgeLessThanEqual(int age);
 
 }
