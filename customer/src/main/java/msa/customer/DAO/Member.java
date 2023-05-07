afa4d77d6480da584cd9f81dcc4548c33276d527
Sijun <sionwer4@naver.com>
2023-05-07 20:30:42 +0900

    refactor(customer): Change DAO to use mongodb

--- customer/src/main/java/msa/customer/DAO/Member.java
+++ customer/src/main/java/msa/customer/DAO/Member.java
@@ -8,24 +8,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@DynamoDBTable(tableName = "delivery_customer")
+//@DynamoDBTable(tableName = "delivery_customer")
+@Document
 public class Member {
 
-    @DynamoDBHashKey
+    @MongoId
     private String memberId;
-    @DynamoDBAttribute
     private String name;
-    @DynamoDBAttribute
     private String email;
-    @DynamoDBAttribute
     private String phoneNumber;
-    @DynamoDBAttribute
     private String address;
 
 }
--- customer/src/main/java/msa/customer/repository/SpringDataDynamoMemberRepository.java
+++ customer/src/main/java/msa/customer/repository/SpringDataDynamoMemberRepository.java
@@ -4,9 +4,6 @@ import msa.customer.DAO.Member;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 @EnableScan
 public interface SpringDataDynamoMemberRepository extends CrudRepository<Member, String> {
 
