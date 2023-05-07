7b3d994d72ef45ea0ad5b14c6fadbe7ec3641c9a
Sijun <sionwer4@naver.com>
2023-05-07 20:42:35 +0900

    style(customer): Delete Enable repository of dynamodb config

--- customer/src/main/java/msa/customer/CustomerApplication.java
+++ customer/src/main/java/msa/customer/CustomerApplication.java
@@ -1,9 +1,7 @@
 package msa.customer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
