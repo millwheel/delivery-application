3484eac2d57e52922acee049c81f5ba56a580a9e
Sijun <sionwer4@naver.com>
2023-05-07 20:07:55 +0900

    feat(customer): Create MongoDB configuration

---
+++ customer/src/main/java/msa/customer/config/MongoDBConfig.java
@@ -0,0 +1,10 @@
+package msa.customer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "msa.customer.repository")
+public class MongoDBConfig {
+
+}
