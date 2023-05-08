8e8f97de7846bad04b9f156508bc53428e83b7fd
Sijun <sionwer4@naver.com>
2023-05-08 14:50:46 +0900

    refactor(customer): Create interceptor for member join service

--- customer/src/main/java/msa/customer/controller/MemberController.java
+++ customer/src/main/java/msa/customer/controller/MemberController.java
@@ -1,6 +1,7 @@
 package msa.customer.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import msa.customer.DTO.MemberForm;
 import msa.customer.service.JoinService;
 import msa.customer.service.MemberService;
@@ -13,29 +14,15 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/customer")
+@Slf4j
 public class MemberController {
 
     private final ParseJwtService parseJwtService;
     private final MemberService memberService;
-    private final JoinService joinService;
 
-    public MemberController(ParseJwtService parseJwtService, MemberService memberService, JoinService joinService) {
+    public MemberController(ParseJwtService parseJwtService, MemberService memberService) {
         this.parseJwtService = parseJwtService;
         this.memberService = memberService;
-        this.joinService = joinService;
-    }
-
-    @GetMapping("/main-gate")
-    @ResponseStatus(HttpStatus.OK)
-    public String main(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwt,
-                     @RequestParam(defaultValue = "/customer/food-kind") String redirectURL,
-                     HttpServletResponse response) throws IOException {
-        String cognitoUsername = parseJwtService.getCognitoUsernameFromJwt(jwt);
-        if(!joinService.checkJoinedMember(cognitoUsername)){
-            String email = parseJwtService.getEmailFromJwt(jwt);
-            joinService.joinMember(cognitoUsername, email);
-        }
-        return "member main-gate";
     }
 
     @GetMapping("/member/info")
@@ -62,6 +49,6 @@ public class MemberController {
         if(name != null) memberService.setName(id, name);
         if(phoneNumber != null) memberService.setPhoneNumber(id, phoneNumber);
         if(address != null) memberService.setAddress(id, address);
-        response.sendRedirect("/customer/main-gate");
+        response.sendRedirect("/customer/member/info");
     }
 }
---
+++ customer/src/main/java/msa/customer/interceptor/JoinCheckInterceptor.java
@@ -0,0 +1,34 @@
+package msa.customer.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import msa.customer.service.JoinService;
+import msa.customer.service.ParseJwtService;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class JoinCheckInterceptor implements HandlerInterceptor {
+
+    private final ParseJwtService parseJwtService;
+    private final JoinService joinService;
+
+    public JoinCheckInterceptor(ParseJwtService parseJwtService, JoinService joinService) {
+        this.parseJwtService = parseJwtService;
+        this.joinService = joinService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        log.info("authorization activated: {}", requestURI);
+        String jwt = request.getHeader("Authorization");
+        String cognitoUsername = parseJwtService.getCognitoUsernameFromJwt(jwt);
+        if(!joinService.checkJoinedMember(cognitoUsername)){
+            String email = parseJwtService.getEmailFromJwt(jwt);
+            joinService.joinMember(cognitoUsername, email);
+            log.info("Create member id={}, email={}", cognitoUsername, email);
+        }
+        return true;
+    }
+}
