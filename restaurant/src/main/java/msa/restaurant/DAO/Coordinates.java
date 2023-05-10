b005a6467cc2a41733fd8a837f769d610ea6857b
Sijun <sionwer4@naver.com>
2023-05-10 10:53:00 +0900

    feat(restaurant): Create coordinate data model

---
+++ restaurant/src/main/java/msa/restaurant/DAO/Coordinates.java
@@ -0,0 +1,15 @@
+package msa.restaurant.DAO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordinates {
+    private String x;
+    private String y;
+}
--- restaurant/src/main/java/msa/restaurant/DAO/Member.java
+++ restaurant/src/main/java/msa/restaurant/DAO/Member.java
@@ -1,24 +1,27 @@
 package msa.restaurant.DAO;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import java.util.List;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Document("member")
 public class Member {
-    @Id
-    private String id;
+
+    @MongoId
+    private String memberId;
     private String name;
     private String email;
-    private String password;
-    private List<String> store;
-    public Member(){
-
-    }
+    private String phoneNumber;
+    private String address;
+    private String addressDetail;
+    private Coordinates coordinates;
 
 }
