ae362f56820789246ac600b5cda199fe0b15ff3c
Sijun <sionwer4@naver.com>
2023-05-10 11:20:37 +0900

    feat(restaurant): Create join check interceptor

---
+++ restaurant/src/main/java/msa/restaurant/config/WebConfig.java
@@ -0,0 +1,28 @@
+package msa.restaurant.config;
+
+import msa.restaurant.interceptor.JoinCheckInterceptor;
+import msa.restaurant.service.JoinService;
+import msa.restaurant.service.ParseJwtService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ParseJwtService parseJwtService;
+    private final JoinService joinService;
+
+    public WebConfig(ParseJwtService parseJwtService, JoinService joinService) {
+        this.parseJwtService = parseJwtService;
+        this.joinService = joinService;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JoinCheckInterceptor(parseJwtService, joinService))
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/customer", "/customer/main", "/error", "/weather");
+    }
+}
---
+++ restaurant/src/main/java/msa/restaurant/interceptor/JoinCheckInterceptor.java
@@ -0,0 +1,32 @@
+package msa.restaurant.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import msa.restaurant.service.JoinService;
+import msa.restaurant.service.ParseJwtService;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class JoinCheckInterceptor implements HandlerInterceptor {
+
+    private final ParseJwtService parseJwtService;
+    private final JoinService joinService;
+
+    public JoinCheckInterceptor(ParseJwtService parseJwtService, JoinService joinService) {
+        this.parseJwtService = parseJwtService;
+        this.joinService = joinService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String jwt = request.getHeader("Authorization");
+        String cognitoUsername = parseJwtService.getCognitoUsernameFromJwt(jwt);
+        if(!joinService.checkJoinedMember(cognitoUsername)){
+            String email = parseJwtService.getEmailFromJwt(jwt);
+            joinService.joinMember(cognitoUsername, email);
+            log.info("Create member: id={}, email={}", cognitoUsername, email);
+        }
+        return true;
+    }
+}
