445be3bb9a04231f3ce98fa852e46e1589d376cb
Sijun <sionwer4@naver.com>
2023-05-10 11:49:52 +0900

    feat(restaurant): Create menu data model

---
+++ restaurant/src/main/java/msa/restaurant/DAO/Menu.java
@@ -0,0 +1,20 @@
+package msa.restaurant.DAO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("menu")
+public class Menu {
+
+    private String name;
+    private String price;
+    private String description;
+}
--- restaurant/src/main/java/msa/restaurant/controller/MenuController.java
+++ restaurant/src/main/java/msa/restaurant/controller/MenuController.java
@@ -1,9 +1,6 @@
 package msa.restaurant.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import msa.restaurant.service.MemberService;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
