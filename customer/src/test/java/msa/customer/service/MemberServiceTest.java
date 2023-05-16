6c083d49de87ff1f8a4d5d6ed10103af4f1f8fc2
Sijun <sionwer4@naver.com>
2023-05-16 15:42:15 +0900

    test(customer): Add test of member service

---
+++ customer/src/test/java/msa/customer/service/MemberServiceTest.java
@@ -0,0 +1,73 @@
+package msa.customer.service;
+
+import msa.customer.DAO.Coordinates;
+import msa.customer.DAO.Member;
+import msa.customer.DTO.MemberForm;
+import msa.customer.repository.member.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    private MemberRepository memberRepository;
+    private MemberService memberService;
+    private JoinService joinService;
+    private final String ID = "1315-0045-7784-0159";
+    private final String NAME = "John";
+    private final String EMAIL = "john@onemail.co.kr";
+    private final String PHONE_NUMBER = "01023459988";
+    private final String ADDRESS = "서울시 강남구 테헤란로 131";
+    private final String ADDRESS_DETAIL = "first floor";
+    private final Coordinates COORDINATES = new Coordinates("127.032937953168", "37.5000818732753");
+
+
+    @Autowired
+    MemberServiceTest(MemberRepository memberRepository, MemberService memberService, JoinService joinService) {
+        this.memberRepository = memberRepository;
+        this.memberService = memberService;
+        this.joinService = joinService;
+    }
+
+    @DisplayName("회원 정보 조회")
+    @Test
+    void getMemberInfo(){
+        // given
+        Member member = new Member();
+        member.setMemberId(ID);
+        member.setName(NAME);
+        member.setEmail(EMAIL);
+        member.setPhoneNumber(PHONE_NUMBER);
+        member.setAddress(ADDRESS);
+        member.setAddressDetail(ADDRESS_DETAIL);
+        // when
+        MemberForm userInfo = memberService.getUserInfo(ID);
+        // then
+        assertThat(userInfo.getName()).isEqualTo(member.getName());
+        assertThat(userInfo.getEmail()).isEqualTo(member.getEmail());
+        assertThat(userInfo.getPhoneNumber()).isEqualTo(member.getPhoneNumber());
+        assertThat(userInfo.getAddress()).isEqualTo(member.getAddress());
+        assertThat(userInfo.getAddressDetail()).isEqualTo(member.getAddressDetail());
+    }
+
+    @Test
+    void setAddress() {
+        // given
+        joinService.joinMember(ID, EMAIL);
+        // when
+        memberService.setAddress(ID, ADDRESS);
+        // then
+        assertThat(memberService.getAddress(ID).get()).isEqualTo(ADDRESS);
+        assertThat(memberService.getCoordinates(ID).get()).isEqualTo(COORDINATES);
+    }
+
+    @Test
+    void changeUserInfo() {
+
+    }
+}
