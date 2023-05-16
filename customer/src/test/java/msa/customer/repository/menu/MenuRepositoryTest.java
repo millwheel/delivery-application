cf144cb36e8b4253f3cd70bde594ffc37eca0052
Sijun <sionwer4@naver.com>
2023-05-16 17:13:40 +0900

    test(customer): Create test for menu saving

--- customer/src/test/java/msa/customer/repository/menu/MenuRepositoryTest.java
+++ customer/src/test/java/msa/customer/repository/menu/MenuRepositoryTest.java
@@ -2,11 +2,13 @@ package msa.customer.repository.menu;
 
 import msa.customer.DAO.Member;
 import msa.customer.DAO.Menu;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -25,12 +27,11 @@ class MenuRepositoryTest {
         Menu menu = new Menu();
         menu.setName("불고기 피자");
         menu.setPrice(18900);
-
         // when
         String id = menuRepository.make(menu);
-
+        Menu storedMenu = menuRepository.findById(id).get();
         // then
-        System.out.printf("mongo _id = %s\n", id);
-
+        assertThat(storedMenu.getName()).isEqualTo(menu.getName());
+        assertThat(storedMenu.getPrice()).isEqualTo(menu.getPrice());
     }
 }
