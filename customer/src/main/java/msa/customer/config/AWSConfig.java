8ba37328c0fcc68e38ff63b22ad3cda6d3bec5b4
Sijun <sionwer4@naver.com>
2023-05-24 11:16:41 +0900

    feat(customer): Add aws sns sqs configuration

---
+++ customer/src/main/java/msa/customer/config/AWSConfig.java
@@ -0,0 +1,58 @@
+package msa.customer.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableDynamoDBRepositories(basePackages = "msa.customer.repository")
+public class AWSConfig {
+
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+    @Value("${aws.region}")
+    private String awsRegion;
+    @Value("${aws.sns.topic.arn}")
+    private String snsTopicARN;
+
+    public AWSCredentials awsCredentials() {
+        return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    }
+
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(awsCredentials());
+    }
+
+    // DynamoDB Configuration
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard().withCredentials(awsCredentialsProvider())
+                .withRegion(awsRegion).build();
+    }
+
+    // SNS Configuration
+    @Bean
+    public AmazonSNS amazonSNS(){
+        return AmazonSNSClientBuilder.standard().withCredentials(awsCredentialsProvider())
+                .withRegion(awsRegion).build();
+    }
+
+    // SQS Configuration
+    @Bean
+    public AmazonSQS amazonSQS(){
+        return AmazonSQSClientBuilder.standard().withCredentials(awsCredentialsProvider())
+                .withRegion(awsRegion).build();
+    }
+}
