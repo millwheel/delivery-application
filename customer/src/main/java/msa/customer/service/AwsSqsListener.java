307d155b1dab0706988323208c9b399b1e66aecc
Sijun <sionwer4@naver.com>
2023-05-28 20:47:06 +0900

    refactor(customer): Change dependency of sqs java sdk

--- customer/src/main/java/msa/customer/service/AwsSqsListener.java
+++ customer/src/main/java/msa/customer/service/AwsSqsListener.java
@@ -3,8 +3,6 @@ package msa.customer.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Slf4j
 @Service
 public class AwsSqsListener {
--- customer/src/main/java/msa/customer/service/AwsSqsService.java
+++ customer/src/main/java/msa/customer/service/AwsSqsService.java
@@ -8,8 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 @Slf4j
 public class AwsSqsService {
