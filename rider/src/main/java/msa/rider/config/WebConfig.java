5f0b50d2090605a85d1126a84dd17134f3b74226
Sijun <sionwer4@naver.com>
2023-06-05 15:22:46 +0900

    feat(rider): Create web interceptor configuration

---
+++ rider/src/main/java/msa/rider/config/WebConfig.java
@@ -0,0 +1,28 @@
+package msa.rider.config;
+
+import msa.rider.interceptor.JoinCheckInterceptor;
+import msa.rider.service.JoinService;
+import msa.rider.service.ParseJwtService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ParseJwtService parseJwtService;
+    private final JoinService joinService;
+
+    public WebConfig(ParseJwtService parseJwtService, JoinService joinService) {
+        this.parseJwtService = parseJwtService;
+        this.joinService = joinService;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JoinCheckInterceptor(parseJwtService, joinService))
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/customer", "/customer/main", "/error");
+    }
+}
