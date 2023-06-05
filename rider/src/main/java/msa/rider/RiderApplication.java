6850e2ec9a1e6a6160e0ea84baa5cd007f3137f8
Sijun <sionwer4@naver.com>
2023-06-05 15:31:13 +0900

    feat(rider): Create aws sqs service

--- rider/src/main/java/msa/rider/RiderApplication.java
+++ rider/src/main/java/msa/rider/RiderApplication.java
@@ -2,7 +2,9 @@ package msa.rider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RiderApplication {
 
