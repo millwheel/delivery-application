b94c129799ca7ff5d4dbf14a2782c9a1ed296dab
Sijun <sionwer4@naver.com>
2023-06-05 19:56:16 +0900

    feat(restaurant): Create sqs service to send message to customer and rider seperately

--- restaurant/src/main/java/msa/restaurant/controller/SqsController.java
+++ restaurant/src/main/java/msa/restaurant/controller/SqsController.java
@@ -1,7 +1,7 @@
 package msa.restaurant.controller;
 
 import com.amazonaws.services.sqs.model.SendMessageResult;
-import msa.restaurant.service.AwsSqsService;
+import msa.restaurant.service.SqsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/manager")
 public class SqsController {
 
-    private final AwsSqsService awsSqsService;
+    private final SqsService sqsService;
 
-    public SqsController(AwsSqsService awsSqsService) {
-        this.awsSqsService = awsSqsService;
+    public SqsController(SqsService sqsService) {
+        this.sqsService = sqsService;
     }
 
-    @PostMapping("/send")
-    public SendMessageResult send(@RequestBody String message) {
-        return awsSqsService.send(message);
-    }
 
 }
--- restaurant/src/main/java/msa/restaurant/service/SqsService.java
+++ restaurant/src/main/java/msa/restaurant/service/SqsService.java
@@ -2,7 +2,6 @@ package msa.restaurant.service;
 
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,32 +9,39 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class AwsSqsService {
+public class SqsService {
 
-    @Value("${aws.sqs.url}")
-    private String sqsUrl;
-    @Value("${aws.sqs.name}")
-    private String sqsName;
+    @Value("${aws.sqs.url.customer}")
+    private String customerUrl;
+    @Value("${aws.sqs.url.restaurant}")
+    private String restaurantUrl;
+    @Value("${aws.sqs.url.rider}")
+    private String riderUrl;
 
     private final AmazonSQS amazonSQSClient;
 
-    public AwsSqsService(AmazonSQS amazonSQSClient) {
+    public SqsService(AmazonSQS amazonSQSClient) {
         this.amazonSQSClient = amazonSQSClient;
     }
 
-    public SendMessageResult send(String data){
-        SendMessageRequest sendMessageRequest = new SendMessageRequest(sqsUrl, data);
+    public SendMessageResult sendToCustomer(String data){
+        SendMessageRequest sendMessageRequest = new SendMessageRequest(customerUrl, data);
+        return amazonSQSClient.sendMessage(sendMessageRequest);
+    }
+
+    public SendMessageResult sendToRider(String data){
+        SendMessageRequest sendMessageRequest = new SendMessageRequest(riderUrl, data);
         return amazonSQSClient.sendMessage(sendMessageRequest);
     }
 
     @Scheduled(fixedDelay = 1000)
     public void receive(){
         try{
-            ReceiveMessageResult receiveMessageResult = amazonSQSClient.receiveMessage(sqsUrl);
+            ReceiveMessageResult receiveMessageResult = amazonSQSClient.receiveMessage(restaurantUrl);
             if(!receiveMessageResult.getMessages().isEmpty()){
                 Message message = receiveMessageResult.getMessages().get(0);
                 log.info("message body={}", message.getBody());
-                amazonSQSClient.deleteMessage(sqsUrl, message.getReceiptHandle());
+                amazonSQSClient.deleteMessage(restaurantUrl, message.getReceiptHandle());
             }
         } catch (QueueDoesNotExistException e){
             log.error("Queue Dose not exist {}", e.getMessage());
