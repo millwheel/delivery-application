61922ec3ef754882c377818d2eabbb52c08f1c67
Sijun <sionwer4@naver.com>
2023-06-05 15:37:36 +0900

    feat(rider): Create member controller

---
+++ rider/src/main/java/msa/rider/controller/MemberController.java
@@ -0,0 +1,38 @@
+package msa.rider.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import msa.rider.DTO.RiderForm;
+import msa.rider.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@Slf4j
+@RequestMapping("/manager/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/info")
+    @ResponseStatus(HttpStatus.OK)
+    public RiderForm showMemberInfo(@RequestAttribute("cognitoUsername") String id){
+        return memberService.getUserInfo(id);
+    }
+
+    @PutMapping("/info")
+    @ResponseStatus(HttpStatus.SEE_OTHER)
+    public void updateMemberInfo(@RequestAttribute("cognitoUsername") String id,
+                                 @RequestBody RiderForm data,
+                                 HttpServletResponse response) throws IOException {
+        memberService.updateUserInfo(id, data);
+        response.sendRedirect("/manager/member/info");
+    }
+
+}
