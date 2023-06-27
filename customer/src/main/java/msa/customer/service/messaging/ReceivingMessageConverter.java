41945e9bc47db4a6a471f22471c13f19cd8cedf2
Sijun <sionwer4@naver.com>
2023-06-27 20:05:49 +0900

    refactore(customer): Change message converter to extract store id from data of jsonobject

--- customer/src/main/java/msa/customer/service/messaging/ReceivingMessageConverter.java
+++ customer/src/main/java/msa/customer/service/messaging/ReceivingMessageConverter.java
@@ -43,10 +43,12 @@ public class ReceivingMessageConverter {
             StoreSqsDto storeSqsDto = convertStoreData(jsonObject);
             storeService.updateStore(storeSqsDto);
         } else if (jsonObject.get("method").equals("open")){
-            String storeId = (String)jsonObject.get("storeId");
+            JSONObject data = new JSONObject(jsonObject.get("data").toString());
+            String storeId = (String) data.get("storeId");
             storeService.openStore(storeId);
         } else if (jsonObject.get("method").equals("close")){
-            String storeId = (String)jsonObject.get("storeId");
+            JSONObject data = new JSONObject(jsonObject.get("data").toString());
+            String storeId = (String) data.get("storeId");
             storeService.closeStore(storeId);
         } else if (jsonObject.get("method").equals("delete")) {
             JSONObject data = new JSONObject(jsonObject.get("data").toString());
