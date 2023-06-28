eae78ace9c521c30564b192d241f6fa336c59fde
Sijun <sionwer4@naver.com>
2023-06-28 10:38:59 +0900

    feat(rider): Add change order status fuctnio to order controller

--- rider/src/main/java/msa/rider/controller/OrderController.java
+++ rider/src/main/java/msa/rider/controller/OrderController.java
@@ -1,15 +1,15 @@
 package msa.rider.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import msa.rider.dto.order.OrderResponseDto;
 import msa.rider.entity.order.Order;
+import msa.rider.entity.order.OrderStatus;
 import msa.rider.service.messaging.SendingMessageConverter;
 import msa.rider.service.messaging.SqsService;
 import msa.rider.service.order.OrderService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @RequestMapping("/rider/order")
@@ -36,5 +36,19 @@ public class OrderController {
         return new OrderResponseDto(orderOptional.get());
     }
 
+    @PostMapping("/{orderId}")
+    public void changeOrderStatus(@PathVariable String orderId,
+                                  HttpServletResponse response) throws IOException {
+        Optional<Order> orderOptional = orderService.getOrder(orderId);
+        if (orderOptional.isEmpty()){
+            throw new RuntimeException("order doesn't exist.");
+        }
+        Order order = orderOptional.get();
+        OrderStatus changedOrderStatus = orderService.changeOrderStatusFromClient(orderId, order.getOrderStatus());
+        String messageToUpdateOrderStatus = sendingMessageConverter.createMessageToChangeOrderStatus(orderId, changedOrderStatus);
+        sqsService.sendToRestaurant(messageToUpdateOrderStatus);
+        sqsService.sendToCustomer(messageToUpdateOrderStatus);
+        response.sendRedirect("/restaurant/order" + orderId);
+    }
 
 }
