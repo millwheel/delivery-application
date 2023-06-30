532d18da60d739c8a2d6e8613a25505403c217f8
Sijun <sionwer4@naver.com>
2023-06-30 20:48:20 +0900

    fix(restaurant): Change MenuInbasket to menuInbasketList in desirializer

--- restaurant/src/main/java/msa/restaurant/deserializer/OrderDeserializer.java
+++ restaurant/src/main/java/msa/restaurant/deserializer/OrderDeserializer.java
@@ -38,7 +38,7 @@ public class OrderDeserializer extends StdDeserializer {
         double cy = node.get("customerLocation").get("y").asDouble();
         Point customerLocation = new Point(cx, cy);
         List<OrderMenu> orderMenuList = new ArrayList<>();
-        node.get("MenuInBasket").forEach(eachData -> {
+        node.get("menuInBasketList").forEach(eachData -> {
             OrderMenu orderMenu = new OrderMenu();
             orderMenu.setMenuId(eachData.get("menuId").asText());
             orderMenu.setMenuName(eachData.get("menuName").asText());
