0b2e90454361e980f4d2a3e65a282c2d1de6ac30
Sijun <sionwer4@naver.com>
2023-06-30 18:42:54 +0900

    fix(rider): Add noargconstructor to rider response dto

--- rider/src/main/java/msa/rider/dto/rider/RiderResponseDto.java
+++ rider/src/main/java/msa/rider/dto/rider/RiderResponseDto.java
@@ -1,18 +1,19 @@
 package msa.rider.dto.rider;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import msa.rider.entity.member.Rider;
 import org.springframework.data.geo.Point;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class RiderResponseDto {
     private String name;
     private String email;
     private String phoneNumber;
     private String address;
-    private String addressDetail;
     private Point location;
 
     public RiderResponseDto(Rider rider) {
