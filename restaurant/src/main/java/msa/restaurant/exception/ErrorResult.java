17bf5440e7f325a743d7095d13fc1f21a63d4a2b
Sijun <sionwer4@naver.com>
2023-07-03 16:04:31 +0900

    feat(restaurant): Create error result

---
+++ restaurant/src/main/java/msa/restaurant/exception/ErrorResult.java
@@ -0,0 +1,16 @@
+package msa.restaurant.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ErrorResult {
+    private String code;
+    private String message;
+
+    public ErrorResult(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
--- restaurant/src/main/java/msa/restaurant/exception/ExceptionControllerAdvice.java
+++ restaurant/src/main/java/msa/restaurant/exception/ExceptionControllerAdvice.java
@@ -1,2 +1,18 @@
-package msa.restaurant.exception;public class ExceptionControllerAdvice {
+package msa.restaurant.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "msa.restaurant.controller")
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResult exceptionHandler(Exception e){
+        return new ErrorResult("SERVER ERROR", e.getMessage());
+    }
 }
