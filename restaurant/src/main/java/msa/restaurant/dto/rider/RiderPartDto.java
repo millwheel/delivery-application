fb6e5a6922db48c4b6a42142ed4908cfe81a7c7e
Sijun <sionwer4@naver.com>
2023-07-24 14:48:14 +0900

    fix(restaurant): Add object mapper to deserialize assign order json data

--- restaurant/src/main/java/msa/restaurant/dto/rider/RiderPartDto.java
+++ restaurant/src/main/java/msa/restaurant/dto/rider/RiderPartDto.java
@@ -1,5 +1,7 @@
 package msa.restaurant.dto.rider;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,5 +13,4 @@ public class RiderPartDto {
     private String riderId;
     private String name;
     private String phoneNumber;
-
 }
--- restaurant/src/main/java/msa/restaurant/entity/order/OrderStatus.java
+++ restaurant/src/main/java/msa/restaurant/entity/order/OrderStatus.java
@@ -1,5 +1,9 @@
 package msa.restaurant.entity.order;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum OrderStatus {
+
     ORDER_REQUEST, ORDER_ACCEPT, ORDER_DENY, RIDER_ASSIGNED, FOOD_READY, DELIVERY_IN_PROGRESS, DELIVERY_COMPLETE, ORDER_CANCELED
+
 }
