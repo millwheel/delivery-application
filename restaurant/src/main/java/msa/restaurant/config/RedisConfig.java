67defbcd74f145c95638b293128525c5f98a51f6
Sijun <sionwer4@naver.com>
2023-07-27 15:49:06 +0900

    feat(restaurant): Create redis configuration

---
+++ restaurant/src/main/java/msa/restaurant/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package msa.restaurant.config;
+
+import msa.restaurant.sse.SseService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    public String host;
+    @Value("${redis.port}")
+    public int port;
+    @Value("${redis.username}")
+    public String userName;
+    @Value("${redis.password}")
+    public String password;
+
+    private final SseService sseService;
+
+    public RedisConfig(SseService sseService) {
+        this.sseService = sseService;
+    }
+
+    @Bean
+    public RedisConnectionFactory connectToRedisCloud(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setUsername(userName);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectToRedisCloud());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(new SubService(sseService));
+    }
+
+    // To use pub sub
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectToRedisCloud());
+        container.addMessageListener(messageListenerAdapter(), new ChannelTopic("topic1"));
+        return container;
+    }
+}
