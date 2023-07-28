3e2bf9a9f4025b6073fa73011d0b0b74b23885b1
Sijun <sionwer4@naver.com>
2023-07-28 12:00:56 +0900

    feat(rider): Add redis configuration

--- rider/src/main/java/msa/rider/config/RedisConfig.java
+++ rider/src/main/java/msa/rider/config/RedisConfig.java
@@ -1,2 +1,44 @@
-package msa.rider.config;public class RedisConfig {
+package msa.rider.config;
+
+import msa.rider.sse.SseService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    public String host;
+    @Value("${redis.port}")
+    public int port;
+    @Value("${redis.username}")
+    public String userName;
+    @Value("${redis.password}")
+    public String password;
+
+    @Bean
+    public RedisConnectionFactory connectToRedisCloud(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setUsername(userName);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectToRedisCloud());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
 }
