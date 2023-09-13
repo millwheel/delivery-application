b09ac19f96e709f09edbad5c71cdf0d7c6293ad9
season <sionwer4@naver.com>
2023-09-13 19:57:35 +0900

    refactor(customer): Create custom menu empty exception

--- customer/src/main/java/msa/customer/exception/StoreEmptyException.java
+++ customer/src/main/java/msa/customer/exception/StoreEmptyException.java
@@ -2,7 +2,7 @@ package msa.customer.exception;
 
 public class StoreEmptyException extends RuntimeException {
 
-    private static final String EMPTY_STORE_MESSAGE = "Store is empty. (storeId = %s)";
+    private static final String EMPTY_STORE_MESSAGE = "This store doesn't exist. (storeId = %s)";
 
     public StoreEmptyException(String storeId) {
         super(String.format(EMPTY_STORE_MESSAGE, storeId));
