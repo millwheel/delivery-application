5caf561978e3558963b301e32dedc6bf19a2bd84
season <sionwer4@naver.com>
2023-09-13 20:01:20 +0900

    refactor(customer): Create custom store mismatch exception

---
+++ customer/src/main/java/msa/customer/exception/StoreMismatchException.java
@@ -0,0 +1,10 @@
+package msa.customer.exception;
+
+public class StoreMismatchException extends RuntimeException{
+
+    private static final String STORE_MISMATCH_MESSAGE = "store id: %s doesn't match existing store id";
+
+    public StoreMismatchException(String storeId) {
+        super(String.format(STORE_MISMATCH_MESSAGE, storeId));
+    }
+}
